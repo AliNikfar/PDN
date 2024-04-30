1976a4f1a0f4a3d45387ffc079334cc270191746
AliHome <ali_nikfar2000@yahoo.com>
2024-04-30 14:21:50 -0700

    Add CommandRepo,BaseEntity and IqueryRepo to Shared

---
+++ Shared/PDN.Shared.Abstraction/Commans/ICommandRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDN.Shared.Abstraction.Commans
+{
+    public interface ICommandRepo<T, TID>
+    {
+        Task<T?> FindAsync(TID id);
+
+        Task<TID> AddAsync(T entity);
+    }
+}
